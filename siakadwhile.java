6cabfe9880c84fe4f2717fc4e02b9fcfb1531980
Dhani <dhani@gmail.com>
2025-11-07 10:25:19 +0700

    siakadwhile

---
+++ siakadwhile.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class siakadwhile {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int nilai,jml,i;
+        i=0;
+
+        System.out.println("Masukan Jumlah Mahasiswa: ");
+        jml= s.nextInt();
+        while (i<jml) {
+            System.out.println("Masukan Nilai Mahasiswa: ");
+            nilai = s.nextInt();
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai Tidak Valid, Masukan Nilai Ynag Valid");
+                continue;
+            }
+
+            if (nilai > 80 && nilai <= 100){
+                System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah A");
+            }
+            else if (nilai > 73 && nilai <=80) {
+                System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah B+");
+            }
+            else if (nilai > 65 && nilai <=73) {
+                System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah B");
+            }
+            else if (nilai > 60 && nilai <=65) {
+                System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah C+");
+            }
+            else if (nilai > 50 && nilai <=60) {
+                System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah C");
+            }
+            else if (nilai > 39 && nilai <=80) {
+                System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah D");
+            }
+            else{
+                 System.out.println("Nilai Mahasiswa Ke- "+( i+1)+" adalah E");
+            }
+
+        }
+    }
+}
