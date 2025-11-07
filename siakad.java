49aadd3d1807c5489ac024ab8d92ea5daba1c95a
Dhani <dhani@gmail.com>
2025-11-07 09:42:27 +0700

    modifikasi2

--- siakad.java
+++ siakad.java
@@ -3,6 +3,7 @@ public class siakad {
     public static void main(String[] args) {
         Scanner s =new Scanner(System.in);
         double nilai,tertinggi=0,terendah=100;
+        int lulus=0,tidaklulus=0;
     
 
         for(int i=1;i<=10;i++){
@@ -14,9 +15,17 @@ public class siakad {
             if (nilai<terendah) {
                 terendah=nilai;
             }   
+            if (nilai>=60) {
+                lulus++;
+            }
+            else{
+                tidaklulus++;
+            }
         }
         System.out.println("Nilai Tertinggi: "+tertinggi);
         System.out.println("Nilai Terendah: "+terendah);
+        System.out.println("Jumlah Mahasiswa yang lulus: "+ lulus);
+        System.out.println("Jumlah Mahasiswa yang tidak lulus: "+tidaklulus);
     }
     
 }
